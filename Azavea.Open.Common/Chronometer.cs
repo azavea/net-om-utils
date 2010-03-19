5522258070833e84b120fded8973eca61d987bd1
schnozmeiser <schnozmeiser@45700cc0-520f-4de9-9e16-3c9caeb65f5b>
2010-03-19 18:34:38 +0000

    Added different reporting types to Chronometer, as well as optional extended stats.

--- Azavea.Open.Common/Chronometer.cs
+++ Azavea.Open.Common/Chronometer.cs
@@ -42,6 +42,24 @@ namespace Azavea.Open.Common
         DateTime _startTime;
 		DateTime _lastSplitTime;
 
+		/// <summary>
+		/// Chronometer reporting enumeration.
+		/// </summary>
+		public enum ReportType {
+			/// <summary>
+			/// Display output as a text table.
+			/// </summary>
+			TextTable,
+			/// <summary>
+			/// Generate XML fragments for each key.
+			/// </summary>
+			XmlElements,
+			/// <summary>
+			/// Generate a CSV list for each key.
+			/// </summary>
+			TextCsv
+		}
+
         /// <summary>
         /// Creates (and starts) the timer.
         /// </summary>
@@ -156,15 +174,8 @@ namespace Azavea.Open.Common
         /// The total times for all the operations that have been "ended" via
         /// the static methods at least once.
         /// </summary>
-        private readonly static Dictionary<string, long> _operationTotalTimes =
-            new Dictionary<string, long>();
-
-        /// <summary>
-        /// The number of times each operation has been "ended" via the static
-        /// methods.
-        /// </summary>
-        private readonly static Dictionary<string, int> _operationTotalCounts =
-            new Dictionary<string, int>();
+        private readonly static Dictionary<string, List<long>> _operationTotalTimes =
+            new Dictionary<string, List<long>>();
 
         /// <summary>
         /// Starts timing the specified named operation.  Each time the same
@@ -274,18 +285,15 @@ namespace Azavea.Open.Common
             {
                 lock (_operationTotalTimes)
                 {
-                    if (_operationTotalTimes.ContainsKey(originalKey) &&
-                        _operationTotalCounts.ContainsKey(originalKey))
+                    if (_operationTotalTimes.ContainsKey(originalKey))
                     {
                         // Add the duration and increment the count.
-                        _operationTotalTimes[originalKey] = _operationTotalTimes[originalKey] + duration;
-                        _operationTotalCounts[originalKey] = _operationTotalCounts[originalKey] + 1;
+                        _operationTotalTimes[originalKey].Add(duration);
                     }
                     else
                     {
                         // Not there yet, so just add the first values.
-                        _operationTotalTimes[originalKey] = duration;
-                        _operationTotalCounts[originalKey] = 1;
+                        _operationTotalTimes[originalKey] = new List<long>(new []{duration});
                     }
                 }
             }
@@ -298,33 +306,305 @@ namespace Azavea.Open.Common
         /// or may not have a time reported.  It should,
         /// however, give a rough idea what operations are taking how long.
         /// </summary>
-        public static void ReportTimes()
+		/// <param name="printDisclaimer">Print a disclaimer notice along with the times.</param>
+		/// <param name="extendedStats">Show extended stats: Min, Max, StdDev, Variance.</param>
+		/// <param name="outType">The type of output to report.</param>
+        public static void ReportTimes(bool extendedStats, ReportType outType, bool printDisclaimer)
         {
-            _log.Info("Begin Timing Report:");
-            _log.Info("DISCLAIMER: Operations that ran a very small number of times, yet supposedly");
-            _log.Info("            took a very long time on average, may be ones that blocked on");
-            _log.Info("            another call or operation for a long time.  For example:");
-            _log.Info("            if every row that is read from a database is sent to a stone");
-            _log.Info("            tablet engraver that takes 5 minutes per tablet, and the next");
-            _log.Info("            row isn't read until the first one is finished being written,");
-            _log.Info("            this report will say it took 15 minutes to read 3 rows.  That");
-            _log.Info("            is technically true, but has nothing to do with the database");
-            _log.Info("            or the code.  This report is to give you a general idea where");
-            _log.Info("            to look for performance problems, not to be the One True ");
-            _log.Info("            Benchmarking Tool.");
+			IReporter reporter;
+        	switch (outType)
+        	{
+				case ReportType.XmlElements:
+					reporter = new XmlElementReporter();
+					break;
+				case ReportType.TextCsv:
+					reporter = new TextCsvReporter();
+					break;
+        		//case ReportType.TextTable:
+				default:
+					reporter = new TextTableReporter();
+					break;
+        	}
+
+			reporter.ReportPrologue( printDisclaimer );
+
             lock (_operationTotalTimes)
             {
                 foreach (string key in _operationTotalTimes.Keys)
                 {
-                    long totalTicks = _operationTotalTimes[key];
-                    int count = _operationTotalCounts[key];
-                    TimeSpan average = new TimeSpan(totalTicks / count);
-                    _log.Info("    Total: " + new TimeSpan(totalTicks).ToString().PadRight(16) +
-                        ", Count: " + count.ToString().PadLeft(7) + ", Average: " +
-                        average.ToString().PadRight(16) + ".  Key: " + key);
+                    long totalTicks = 0;
+					long minTicks = Int64.MaxValue;
+					long maxTicks = Int64.MinValue;
+					foreach (long tick in _operationTotalTimes[key])
+					{
+						totalTicks += tick;
+						if ( tick > maxTicks )
+							maxTicks = tick;
+						if ( tick < minTicks )
+							minTicks = tick;
+					}
+                    int count = _operationTotalTimes[key].Count;
+					long average = totalTicks / count;
+					double averageSec = new TimeSpan(average).TotalSeconds;
+					double variance = 0;
+					foreach (long tick in _operationTotalTimes[key])
+					{
+						double sec = new TimeSpan(tick).TotalSeconds;
+						variance += Math.Pow(sec - averageSec,2);
+					}
+					variance /= (count - 1);
+					double stddev = Math.Sqrt(variance);
+
+					reporter.ReportKey( key, count, totalTicks, average, 
+						minTicks, maxTicks, stddev, variance, extendedStats );
                 }
             }
-            _log.Info("End Timing Report.");
+
+			reporter.ReportEpilogue();
         }
-    }
+
+        /// <summary>
+        /// Reports all the times for all the operations that both BeginTiming and
+        /// EndTiming were called for.  This is imperfect as any call that failed may
+        /// or may not have a time reported.  It should,
+        /// however, give a rough idea what operations are taking how long.
+        /// </summary>
+		public static void ReportTimes()
+		{
+			ReportTimes(false, ReportType.TextTable, true);
+		}
+
+        /// <summary>
+        /// Reports all the times for all the operations that both BeginTiming and
+        /// EndTiming were called for.  This is imperfect as any call that failed may
+        /// or may not have a time reported.  It should,
+        /// however, give a rough idea what operations are taking how long.
+        /// </summary>
+		/// <param name="extendedStats">Show extended stats: Min, Max, StdDev, Variance.</param>
+		public static void ReportTimes(bool extendedStats)
+		{
+			ReportTimes(extendedStats, ReportType.TextTable, true);
+		}
+
+        /// <summary>
+        /// Reports all the times for all the operations that both BeginTiming and
+        /// EndTiming were called for.  This is imperfect as any call that failed may
+        /// or may not have a time reported.  It should,
+        /// however, give a rough idea what operations are taking how long.
+        /// </summary>
+		/// <param name="extendedStats">Show extended stats: Min, Max, StdDev, Variance.</param>
+		/// <param name="reportType">Print a disclaimer notice along with the times.</param>
+        public static void ReportTimes(bool extendedStats, ReportType reportType)
+		{
+			ReportTimes(extendedStats, reportType, true);
+		}
+
+		/// <summary>
+		/// Define a private interface for a reported used by ReportTimes.
+		/// </summary>
+		private interface IReporter
+		{
+			void ReportPrologue(bool printDisclaimer);
+			void ReportKey(string key, int count, long totalTicks, long average, long minTicks, long maxTicks, double stddev, double variance, bool extended);
+			void ReportEpilogue();
+		}
+
+		/// <summary>
+		/// A class that writes a nicely human readable text table of timing reports.
+		/// </summary>
+		private class TextTableReporter : IReporter
+		{
+			/// <summary>
+			/// Display a text prologue.
+			/// </summary>
+			/// <param name="printDisclaimer">Show the disclaimer text.</param>
+			public void ReportPrologue(bool printDisclaimer)
+			{
+	            _log.Info("Begin Timing Report:");
+				if ( printDisclaimer ) {
+					_log.Info("DISCLAIMER: Operations that ran a very small number of times, yet supposedly");
+					_log.Info("            took a very long time on average, may be ones that blocked on");
+					_log.Info("            another call or operation for a long time.  For example:");
+					_log.Info("            if every row that is read from a database is sent to a stone");
+					_log.Info("            tablet engraver that takes 5 minutes per tablet, and the next");
+					_log.Info("            row isn't read until the first one is finished being written,");
+					_log.Info("            this report will say it took 15 minutes to read 3 rows.  That");
+					_log.Info("            is technically true, but has nothing to do with the database");
+					_log.Info("            or the code.  This report is to give you a general idea where");
+					_log.Info("            to look for performance problems, not to be the One True ");
+					_log.Info("            Benchmarking Tool.");
+				}
+			}
+
+			/// <summary>
+			/// Report the timing information for 1 key.
+			/// </summary>
+			/// <param name="key">The name of the key.</param>
+			/// <param name="count">The number of times timed.</param>
+			/// <param name="totalTicks">The total time over all times timed.</param>
+			/// <param name="average">The average tick time.</param>
+			/// <param name="minTicks">The minimum tick time.</param>
+			/// <param name="maxTicks">The maximum tick time.</param>
+			/// <param name="stddev">The standard deviation in tick times.</param>
+			/// <param name="variance">The variance in tick times.</param>
+			/// <param name="extended">Show extended stats (min, max, stddev, variance).</param>
+			public void ReportKey( string key, int count, long totalTicks, long average, long minTicks, long maxTicks, double stddev, double variance, bool extended )
+			{
+                TimeSpan averageSpan = new TimeSpan(average);
+				_log.Info(String.Format("    Count: {0},      Key: {1}", count.ToString().PadLeft(16), key));
+                _log.Info(String.Format(
+					"    Total: {0},  Average: {1}",
+					new TimeSpan(totalTicks).ToString().PadRight(16),
+					averageSpan.ToString().PadRight(16))
+				);
+				if ( extended ) {
+					_log.Info(String.Format(
+						"  Minimum: {0},  Maximum: {1}",
+						new TimeSpan(minTicks).ToString().PadRight(16),
+						new TimeSpan(maxTicks).ToString().PadRight(16))
+					);
+					_log.Info(String.Format(
+						"   StdDev: {0}, Variance: {1}",
+						stddev.ToString("F7").PadLeft(16),
+						variance.ToString("F7").PadLeft(16))
+					);
+				}
+			}
+
+			/// <summary>
+			/// Display a text epilogue.
+			/// </summary>
+			public void ReportEpilogue(){
+	            _log.Info("End Timing Report.");
+			}
+		}
+
+		/// <summary>
+		/// A class that writes a nicely machine readable text table of timing reports.
+		/// </summary>
+		private class TextCsvReporter : IReporter
+		{
+			/// <summary>
+			/// Display a text prologue.
+			/// </summary>
+			/// <param name="printDisclaimer">Show the disclaimer text.</param>
+			public void ReportPrologue(bool printDisclaimer)
+			{
+				if ( printDisclaimer ) {
+					_log.Info("\"DISCLAIMER: Operations that ran a very small number of times, yet supposedly " +
+						"took a very long time on average, may be ones that blocked on " +
+						"another call or operation for a long time.  For example: " +
+						"if every row that is read from a database is sent to a stone " +
+						"tablet engraver that takes 5 minutes per tablet, and the next " +
+						"row isn't read until the first one is finished being written, " +
+						"this report will say it took 15 minutes to read 3 rows.  That " +
+						"is technically true, but has nothing to do with the database " +
+						"or the code.  This report is to give you a general idea where " +
+						"to look for performance problems, not to be the One True Benchmarking Tool.\"");
+				}
+			}
+
+			/// <summary>
+			/// Report the timing information for 1 key.
+			/// </summary>
+			/// <param name="key">The name of the key.</param>
+			/// <param name="count">The number of times timed.</param>
+			/// <param name="totalTicks">The total time over all times timed.</param>
+			/// <param name="average">The average tick time.</param>
+			/// <param name="minTicks">The minimum tick time.</param>
+			/// <param name="maxTicks">The maximum tick time.</param>
+			/// <param name="stddev">The standard deviation in tick times.</param>
+			/// <param name="variance">The variance in tick times.</param>
+			/// <param name="extended">Show extended stats (min, max, stddev, variance).</param>
+			public void ReportKey( string key, int count, long totalTicks, long average, long minTicks, long maxTicks, double stddev, double variance, bool extended )
+			{
+				TimeSpan averageSpan = new TimeSpan(average);
+				string extStats = (extended)?
+					String.Format(",{0},{1},{2},{3}",
+						new TimeSpan(minTicks),
+						new TimeSpan(maxTicks),
+						stddev.ToString("F7"),
+						variance.ToString("F7")) :
+					String.Empty;
+				_log.Info(String.Format("\"{0}\",{1},{2},{3}{4}", 
+					key, count,
+					new TimeSpan(totalTicks),
+					averageSpan,
+					extStats));
+			}
+
+			/// <summary>
+			/// Display a text epilogue.
+			/// </summary>
+			public void ReportEpilogue(){
+			}
+		}
+
+		/// <summary>
+		/// A class that writes a nicely machine readable text table of timing reports.
+		/// </summary>
+		private class XmlElementReporter : IReporter
+		{
+			/// <summary>
+			/// Display an xml prologue.
+			/// </summary>
+			/// <param name="printDisclaimer">Show the disclaimer text.</param>
+			public void ReportPrologue(bool printDisclaimer)
+			{
+				if ( printDisclaimer ) {
+					_log.Info("<!-- DISCLAIMER: Operations that ran a very small number of times, yet supposedly\n" +
+					          "                 took a very long time on average, may be ones that blocked on\n" +
+					          "                 another call or operation for a long time.  For example:\n" +
+					          "                 if every row that is read from a database is sent to a stone\n" +
+					          "                 tablet engraver that takes 5 minutes per tablet, and the next\n" +
+					          "                 row isn't read until the first one is finished being written,\n" +
+					          "                 this report will say it took 15 minutes to read 3 rows.  That\n" +
+					          "                 is technically true, but has nothing to do with the database\n" +
+					          "                 or the code.  This report is to give you a general idea where\n" +
+					          "                 to look for performance problems, not to be the One True\n" +
+					          "                 Benchmarking Tool.");
+				}
+			}
+
+			/// <summary>
+			/// Report the timing information for 1 key.
+			/// </summary>
+			/// <param name="key">The name of the key.</param>
+			/// <param name="count">The number of times timed.</param>
+			/// <param name="totalTicks">The total time over all times timed.</param>
+			/// <param name="average">The average tick time.</param>
+			/// <param name="minTicks">The minimum tick time.</param>
+			/// <param name="maxTicks">The maximum tick time.</param>
+			/// <param name="stddev">The standard deviation in tick times.</param>
+			/// <param name="variance">The variance in tick times.</param>
+			/// <param name="extended">Show extended stats (min, max, stddev, variance).</param>
+			public void ReportKey( string key, int count, long totalTicks, long average, long minTicks, long maxTicks, double stddev, double variance, bool extended )
+			{
+				StringBuilder sb = new StringBuilder();
+
+				sb.Append( "<ChronometerTime>" )
+					.Append( "<key value=\"" ).Append( key ).Append("\"/>" )
+					.Append( "<count value=\"" ).Append( count ).Append("\"/>")
+					.Append( "<total value=\"" ).Append( new TimeSpan(totalTicks) ).Append("\"/>")
+					.Append( "<average value\"").Append( new TimeSpan(average) ).Append("\"/>");
+
+				if ( extended ) {
+					sb.Append( "<minmum value=\"" ).Append( new TimeSpan(minTicks)).Append("\"/>")
+						.Append( "<maximum value=\"" ).Append( new TimeSpan(maxTicks) ).Append( "\"/>")
+						.Append( "<standarddeviation value=\"" ).Append( stddev.ToString("F7") ).Append( "\"/>" )
+						.Append( "<variance value=\"").Append( variance.ToString("F7") ).Append("\"/>");
+				}
+
+				sb.Append( "</ChronometerTime>" );
+
+				_log.Info(sb.ToString());
+			}
+
+			/// <summary>
+			/// Display a text epilogue.
+			/// </summary>
+			public void ReportEpilogue(){
+			}
+		}
+	}
 }
